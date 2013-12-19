63fbaa7320d06afd986d0d819c31521f8a76e16c
Mikhail Korobov <kmike84@gmail.com>
2013-12-19 23:36:59 +0600

    add default value for quitAfterSave option

--- xpi/defaults/preferences/defaults.js
+++ xpi/defaults/preferences/defaults.js
@@ -2,3 +2,4 @@ pref("extensions.webannotator.savecolors", false);
 pref("extensions.webannotator.activatelinks", true);
 pref("extensions.webannotator.urisToSave", false);
 pref("extensions.webannotator.saveformat", "add_base");
+pref("extensions.webannotator.quitAfterSave", true);
